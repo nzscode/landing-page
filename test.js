c10788164811073d78c9cbc41df252d1626d8086
Noor Zaqir <nzscode@gmail.com>
2024-04-06 10:49:36 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,18 @@
-const colors = ["red", "green", "blue", "yellow", "orange", "blue", "pink"];
-console.log(colors.lastIndexOf("blue"));
+function greet(nom) {
+    console.log("Hello " + nom);
+}
+greet("Jane");
+
+function add(a, b) {
+    return a + b;
+}
+
+let f = add(3, 2);
+console.log(f);
+
+let arrowGreet = (nom) => console.log("Hello " + nom);
+arrowGreet("Elia");
+
+let arrowAdd = (a, b) => a + b;
+let j = arrowAdd(5, 10);
+console.log(j);

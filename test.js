5b368bc1060edb4e128b6efbce9521cb5fe1f599
Noor Zaqir <nzscode@gmail.com>
2024-04-18 09:06:39 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,7 @@
-function greet(nom) {
-    console.log("Hello " + nom);
+function potatoes(str) {
+    const re = /potato/g;
+    console.log((str || "").match(re) || []).length;
 }
-greet("Jane");
-
-function add(a, b) {
-    return a + b;
-}
-
-let f = add(3, 2);
-console.log(f);
-
-let arrowGreet = (nom) => console.log("Hello " + nom);
-arrowGreet("Elia");
-
-let arrowAdd = (a, b) => a + b;
-let j = arrowAdd(5, 10);
-console.log(j);
+potatoes("potato");
+potatoes("potatopotato");
+potatoes("potatoapple");

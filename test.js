67fda42ecebdcd32a5a5b2bbb82d622234c5d2b2
Noor Zaqir <nzscode@gmail.com>
2024-04-22 23:52:16 -0400

    2024-04-22

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
-function numberSquares(n) {
-    if (n === 1) {
-        return 1;
-    }
-    return n * n + numberSquares(n - 1);
-    console.log(n);
+let button1 = document.querySelector("#button1");
+let banner1 = document.querySelector("#banner1");
+let unorderedList = document.querySelector("#unordered_list");
+console.log(unorderedList.innerText);
+console.log(unorderedList.innerHTML);
+button1.addEventListener("click", printHello);
+function printHello() {
+    button1.innerText = "Button was clicked";
 }

be07ea6b6251d076dbd1b390d26be38eb3fe83ea
Noor Zaqir <nzscode@gmail.com>
2024-06-17 13:51:09 -0400

    2024-06-17

--- test.js
+++
@@ -1,9 +0,0 @@
-let button1 = document.querySelector("#button1");
-let banner1 = document.querySelector("#banner1");
-let unorderedList = document.querySelector("#unordered_list");
-console.log(unorderedList.innerText);
-console.log(unorderedList.innerHTML);
-button1.addEventListener("click", printHello);
-function printHello() {
-    button1.innerText = "Button was clicked";
-}
